cea609853c660b5fbba04ccd31710d6f9335309d
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2022-03-28 13:35:25 +0200

    Update helper_functions.py
    Added open-ended question mapping functionality

--- helper_functions.py
+++ helper_functions.py
@@ -5,6 +5,7 @@ def column_mapping(survey_data) -> list:
         survey_data : pandas dataframe containing survey data
     Returns:
         A list of data columns. They are grouped in the sub-lists for single questions.
+        A list of open-ended questions.
     '''
     
     columns = list(survey_data.columns[1:])
@@ -37,4 +38,21 @@ def column_mapping(survey_data) -> list:
                 temp_list.append(col)
                 mapped_columns.append(temp_list)
                 
-    return mapped_columns
+        # Extracting open-ended questions to a separate list
+        open_ended = []
+
+        for question in mapped_columns:
+            if type(question) == list:
+                for column in question:
+                    if 'other' in column:
+                        open_ended.append(column)
+                        question.remove(column)
+                        if len(question) == 1:
+                            mapped_columns[mapped_columns.index(question)] = question[0]
+            
+            else:
+                if 'other' in question:
+                        open_ended.append(question)
+                        mapped_columns.remove(question)
+                
+    return mapped_columns, open_ended
